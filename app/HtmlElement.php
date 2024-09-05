4bbb6546eb0b16a188c7ba056857d632019d0e34
Alejandra <202100355d@utea.edu.pe>
2024-09-05 16:26:20 -0500

    first commit

---
+++ app/HtmlElement.php
@@ -0,0 +1,42 @@
+<?php  
+namespace App;  
+
+
+class HtmlElement 
+{  
+private $name,$content,$attributes;
+
+
+    public function __construct(string $name,array $attributes=[], $content = null)
+    {
+        $this->attributes =$attributes;
+        $this->name =$name;
+        $this->content=$content;
+        
+    }
+
+    public function render()
+    {
+
+        if (! empty($this->attributes)){
+
+            $htmlAttributes='';
+
+            foreach($this->attributes as $name => $value){
+                $htmlAttributes .= $name.'="'.$value.'"';
+            }
+
+            $result ='<'.$this->name.' '.$htmlAttributes.   '>';
+        }else {
+            $result ='<'.$this->name.'>';
+        }
+       
+       
+
+        $result.= $this->content;
+     
+
+        $result .= '</'.$this->name.'>';
+        return $result;
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php  
+
+    require '../vendor/autoload.php';  
+
+    use App\HtmlElement;  
+
+    $element = new HtmlElement('p',[],'este es el contemido');
